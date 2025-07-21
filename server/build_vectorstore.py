51b41e49e248eb6399a19b2af28d5c3a8a0f50b8
Suhail Khan <suhail.khan@komprise.com>
2025-07-21 14:27:47 +0530

    Fixed build_vectorstore import statement

--- server/build_vectorstore.py
+++ server/build_vectorstore.py
@@ -1,6 +1,6 @@
 # scripts/build_vectorstore.py
 
-from server.app.tools.rag_tool import build_vectorstore_from_sitemap
+from app.tools.rag_tool import build_vectorstore_from_sitemap
 
 if __name__ == "__main__":
     build_vectorstore_from_sitemap("https://www.komprise.com/post-sitemap1.xml")
